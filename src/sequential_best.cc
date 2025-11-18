1483e555f77a183409cae82596bdbf7f1b23ceb8
Chonapatcc <chonapatcz@gmail.com>
2025-11-18 10:52:46 +0700

    fixing best.cc

--- src/sequential_best.cc
+++ src/sequential_best.cc
@@ -45,13 +45,21 @@ struct Tree
     double size;
     Tree* child_particles[8];
 
+    Tree() : COM_x(0), COM_y(0), COM_z(0), mass(0), particle(nullptr), is_leaf(true), has_particle(false), size(0)
+    {
+        for(int i=0;i<8;i++)
+        {
+            child_particles[i] = nullptr; 
+        }
+    }
+
     bool isEmpty()
     {
         return !has_particle;
     }
 };
 
-vector<Particle> particles;
+Particle* particles;
 
 void read_data()
 {
@@ -82,15 +90,39 @@ void read_data()
             else if(_==6) vz = stod(value);
             else if(_==7) mass = stod(value);
         }
-        particles[i] = Particle(idx, x, y, z, vx, vy, vz, mass);
+        new (&particles[i]) Particle(idx, x, y, z, vx, vy, vz, mass);
     }
     
     fin.close();
 }
 
+Tree* allocate_tree()
+{
+    Tree* tree = nullptr;
+
+    tree = (Tree*) malloc (sizeof (Tree));
+
+    new (tree) Tree();
+    return tree;
+}
+
+void delete_tree(Tree* tree)
+{
+    if(tree == nullptr) return;
+
+    if(!tree->is_leaf)
+    {
+        for(int i=0 ;i <8;i++)
+        {
+            delete_tree(tree->child_particles[i]);
+        }
+    }
+    free(tree);
+}
+
 void allocate_memory()
 {
-    particles = vector<Particle>(n);
+    particles = (Particle*) malloc(n * sizeof(Particle));
 }
 
 void show_data()
@@ -129,33 +161,32 @@ void save_force()
     fout.close();
 }
 
-
-
-vector<double> calculate_COM(vector<Particle> &particle_list)
+vector<double> calculate_COM(vector<Particle*> &particle_list)
 {
     double total_mass =0;
     double com_x =0 , com_y =0 , com_z =0 ;
-    for(Particle p : particle_list)
+    for(Particle* p : particle_list)
     {
-        total_mass += p.mass;
-        com_x += p.x * p.mass;
-        com_y += p.y * p.mass;
-        com_z += p.z * p.mass;
+        total_mass += p->mass;
+        com_x += p->x * p->mass;
+        com_y += p->y * p->mass;
+        com_z += p->z * p->mass;
     }
+    if(total_mass ==0 ) return {0,0,0,0};
     com_x /= total_mass;
     com_y /= total_mass;
     com_z /= total_mass;
     return {com_x, com_y, com_z, total_mass};
 }
 
-vector<double> calculate_size(vector<Particle> &particle_list , double center_x , double center_y, double center_z)
+vector<double> calculate_size(vector<Particle*> &particle_list , double center_x , double center_y, double center_z)
 {
     double max_x = 0 , max_y =0, max_z =0 ;
-    for(Particle p : particle_list)
+    for(Particle *p : particle_list)
     {
-        double dx = fabs(p.x - center_x);
-        double dy = fabs(p.y - center_y);
-        double dz = fabs(p.z - center_z);
+        double dx = fabs(p->x - center_x);
+        double dy = fabs(p->y - center_y);
+        double dz = fabs(p->z - center_z);
         if(dx > max_x) max_x = dx;
         if(dy > max_y) max_y = dy;
         if(dz > max_z) max_z = dz;
@@ -163,7 +194,7 @@ vector<double> calculate_size(vector<Particle> &particle_list , double center_x 
     return {max_x, max_y, max_z};
 }
 
-void build_tree(Tree* tree, vector<Particle> &particle_list)
+void build_tree(Tree* tree, vector<Particle*> &particle_list)
 {
     if (particle_list.size() == 0 )
     {
@@ -176,11 +207,11 @@ void build_tree(Tree* tree, vector<Particle> &particle_list)
     {
         tree->is_leaf = true;
         tree->has_particle = true;
-        tree->particle = &particle_list[0];
-        tree->mass = particle_list[0].mass;
-        tree->COM_x = particle_list[0].x;
-        tree->COM_y = particle_list[0].y;
-        tree->COM_z = particle_list[0].z;
+        tree->particle = particle_list[0];
+        tree->mass = particle_list[0]->mass;
+        tree->COM_x = particle_list[0]->x;
+        tree->COM_y = particle_list[0]->y;
+        tree->COM_z = particle_list[0]->z;
         return;
     }
     else
@@ -196,50 +227,47 @@ void build_tree(Tree* tree, vector<Particle> &particle_list)
         vector<double> sizes = calculate_size(particle_list, tree->COM_x, tree->COM_y, tree->COM_z);
         double child_size = max(sizes[0],max(sizes[1],sizes[2]));
         tree->size = child_size * 2;
-        vector<vector<Particle>> child_particles(8);
+        vector<vector<Particle*>> child_particles(8);
 
-        for(Particle p : particle_list)
+        for(Particle *p : particle_list)
         {
             int octant = 0;
-            if(p.x > tree->COM_x) octant |= 1;
-            if(p.y > tree->COM_y) octant |= 2;
-            if(p.z > tree->COM_z) octant |= 4;
+            if(p->x > tree->COM_x) octant |= 1;
+            if(p->y > tree->COM_y) octant |= 2;
+            if(p->z > tree->COM_z) octant |= 4;
 
             child_particles[octant].push_back(p);
         }
         for(int i =0 ;i <8; i++)
         {
-            double offset_x = ((i & 1) ? 0.5 : -0.5) * child_size;
-            double offset_y = ((i & 2) ? 0.5 : -0.5) * child_size;
-            double offset_z = ((i & 4) ? 0.5 : -0.5) * child_size;
-            tree->child_particles[i] = new Tree();
+            tree->child_particles[i] = allocate_tree();
             build_tree(tree->child_particles[i], child_particles[i]);
         }
 
     }
 }
 
-double calculate_distance(Particle &p, Tree* tree)
+double calculate_distance(Particle *p, Tree* tree)
 {
-    double dx = tree->COM_x - p.x;
-    double dy = tree->COM_y - p.y;
-    double dz = tree->COM_z - p.z;
+    double dx = tree->COM_x - p->x;
+    double dy = tree->COM_y - p->y;
+    double dz = tree->COM_z - p->z;
 
     return sqrtf(dx*dx + dy*dy + dz*dz);
 }
 
-void calculate_acceleration(Particle &p, Tree* tree, double& ax, double& ay, double& az)
+void calculate_acceleration(Particle *p, Tree* tree, double& ax, double& ay, double& az)
 {
     if(tree==nullptr || tree->mass == 0) return;
-    if(tree->is_leaf == true && tree->particle == &p) return;
+    if(tree->is_leaf == true && tree->particle == p) return;
 
     double d = calculate_distance(p,tree);
     
     if((tree->size / d) < theta || tree->is_leaf == true)
     {
-        double dx = tree->COM_x - p.x;
-        double dy = tree->COM_y - p.y;
-        double dz = tree->COM_z - p.z;
+        double dx = tree->COM_x - p->x;
+        double dy = tree->COM_y - p->y;
+        double dz = tree->COM_z - p->z;
         double dist_sqr = dx*dx + dy*dy + dz*dz + softening*softening;
                 
         double invDist = 1.0/ sqrtf(dist_sqr);
@@ -264,32 +292,45 @@ void calculate_acceleration(Particle &p, Tree* tree, double& ax, double& ay, dou
 
 }
 
-void nbody()
+void calculate_force(Particle *particle_ptrs,Tree* root)
 {
-    Tree* root = new Tree();
-    build_tree(root,particles);
+    double ax=0,ay=0,az=0;
+
+    calculate_acceleration(particles, root, ax, ay, az);
     
-    double theta = 0.5;
-    for(int i =0 ; i<n ; i++)
-    {
-        double ax=0,ay=0,az=0;
+    particles->vx += ax * dt;
+    particles->vy += ay * dt;
+    particles->vz += az * dt;
 
-        calculate_acceleration(particles[i], root, ax, ay, az);
+    double force = sqrtf(ax*ax + ay*ay + az*az) * particles->mass;
+    particles->force = force;
+}
 
-        particles[i].vx += ax * dt;
-        particles[i].vy += ay * dt;
-        particles[i].vz += az * dt;
-
-        double force = sqrtf(ax*ax + ay*ay + az*az) * particles[i].mass;
-        particles[i].force = force;
-   
-    }
+void update_positions(vector<Particle*>&particles)
+{
     for(int i=0;i<n;i++)
     {
-        particles[i].update_position(dt);
+        particles[i]->update_position(dt);
+    }
+}
+void nbody()
+{
+    vector<Particle*> particle_ptrs(n);
+    for(int i=0 ; i<n ; i++)
+    {
+        particle_ptrs[i] = &particles[i];
     }
 
-    delete root;
+    Tree* root = allocate_tree();
+    build_tree(root,particle_ptrs);
+    
+    
+    for(int i =0 ; i<n ; i++)
+    {
+        calculate_force(particle_ptrs[i], root);
+    }
+    update_positions(particle_ptrs);
+    // delete_tree(root);
 }
 
 void save_time(double time_elapsed)
@@ -317,4 +358,7 @@ int main()
 
     save_time(elapsed.count());
     save_force();
+    
+    free(particles);
+    return 0;
 }
